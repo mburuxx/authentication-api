dbcc0f5a500801382f365254dc85dd1bb6bca584
mburuxx <njihiamburu.3@gmail.com>
2024-07-18 19:28:05 +0300

    Register users and update the database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 /**
  * @desc   Auth user/set token
@@ -17,7 +18,31 @@ const authUser = asyncHandler( async (req, res) => {
  */
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({message: 'User Registred'});
+    const { name, email, password } = req.body;
+    
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error ('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data!');
+    }
 });
 
 /**
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', userRoutes);
 
@@ -14,4 +18,5 @@ app.get('/', (req, res) => res.send('Server is ready'));
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+connectDB();
+app.listen(port, () => console.log(`Server started on port ${port}`))
