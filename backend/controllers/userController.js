c878dc0311ee1d6da44fa382bb5e8edb60a72137
mburuxx <njihiamburu.3@gmail.com>
2024-07-18 16:06:50 +0300

    Run the server and created authentication routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+import asyncHandler from 'express-async-handler';
+
+/**
+ * @desc   Auth user/set token
+ * route   POST /api/users/auth
+ * @access Public
+ */
+
+const authUser = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Authenticated!'});
+});
+
+/**
+ * @desc   Register new user
+ * route   POST /api/users
+ * @access Public
+ */
+
+const registerUser = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Registred'});
+});
+
+/**
+ * @desc   Logout new user
+ * route   POST /api/users/logout
+ * @access Public
+ */
+
+const logoutUser = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Logged out'});
+});
+
+/**
+ * @desc   Get user profile
+ * route   POST /api/users/profile
+ * @access Private (jwt)
+ */
+
+const getUserProfile = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Profile Displayed'});
+});
+
+/**
+ * @desc   Update user profile
+ * route   PUT /api/users
+ * @access Private
+ */
+
+const updateUserProfile = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Profile Updated!'});
+});
+
+export { 
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+ };
