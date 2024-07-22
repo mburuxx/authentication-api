e27b724bae5c5bea13a5754a1d83de3a43be07ef
mburuxx <njihiamburu.3@gmail.com>
2024-07-22 11:20:53 +0300

    Auth protect middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,7 +84,13 @@ const logoutUser = asyncHandler( async (req, res) => {
  */
 
 const getUserProfile = asyncHandler( async (req, res) => {
-    res.status(200).json({message: 'User Profile Displayed'});
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    };
+
+    res.status(200).json(user);
 });
 
 /**
